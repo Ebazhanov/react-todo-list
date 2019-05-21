c70fc3b55e98c97408717ff2e71372b712aafae4
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2019-05-21 23:15:51 +0200

    upd

--- src/components/search-panel.js
+++ src/components/search-panel.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React from 'react';
+
+import './search-panel.css';
 
 const SearchPanel = () => {
-    const searchText = 'Type here to search';
-    const sarchStyle = {
-        fontSize: '20px'
-    };
-    return <input
-        style={sarchStyle}
-        placeholder={searchText}/>
+    return (
+        <input type="text"
+               className="form-control search-input"
+               placeholder="type to search"/>
+    );
 };
 
-export default SearchPanel
+export default SearchPanel;
--- src/components/todo-list-item.js
+++ src/components/todo-list-item.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React from 'react';
+
+import './todo-list-item.css';
 
 const TodoListItem = ({label, important = false}) => {
 
     const style = {
-        color: important ? 'tomato' : 'black'
+        color: important ? 'steelblue' : 'black',
+        fontWeight: important ? 'bold' : 'normal'
     };
 
-    return <span style={style}>{label}</span>
+    return (
+        <span className="todo-list-item">
+      <span
+          className="todo-list-item-label"
+          style={style}>
+        {label}
+      </span>
+
+      <button type="button"
+              className="btn btn-outline-success btn-sm float-right">
+        <i className="fa fa-exclamation"/>
+      </button>
+
+      <button type="button"
+              className="btn btn-outline-danger btn-sm float-right">
+        <i className="fa fa-trash-o"/>
+      </button>
+    </span>
+    );
 };
 
-export default TodoListItem
+export default TodoListItem;
