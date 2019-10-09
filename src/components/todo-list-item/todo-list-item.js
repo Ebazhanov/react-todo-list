2b46e0fe213d8886c0300bc11034ea9f8ecf09f6
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2019-10-09 16:33:52 +0200

    - updates

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -4,6 +4,10 @@ import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
 
+    onLabelClick = () => {
+            console.log(`Done: ${this.props.label}`)
+    };
+
     render() {
 
         const {label, important = false} = this.props;
@@ -17,7 +21,8 @@ export default class TodoListItem extends Component {
             <span className="todo-list-item">
       <span
           className="todo-list-item-label"
-          style={style}>
+          style={style}
+          onClick={this.onLabelClick}>
         {label}
       </span>
 
