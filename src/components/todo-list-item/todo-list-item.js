89c626fa948d56534a152e78b1c1b5150dbcda96
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2019-10-13 11:58:54 +0200

    - :relaxed: updates

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -4,13 +4,27 @@ import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
 
+    state = {
+        done: false
+    };
+
     onLabelClick = () => {
-            console.log(`Done: ${this.props.label}`)
+        this.setState({
+            // cross the the label
+            done: true
+        })
     };
 
     render() {
 
         const {label, important = false} = this.props;
+        // read state
+        const {done} = this.state;
+        let classNames = 'todo-list-item';
+        if(done){
+            // className + 'done'
+            classNames +=' done';
+        }
 
         const style = {
             color: important ? 'steelblue' : 'black',
@@ -18,13 +32,13 @@ export default class TodoListItem extends Component {
         };
 
         return (
-            <span className="todo-list-item">
-      <span
-          className="todo-list-item-label"
-          style={style}
-          onClick={this.onLabelClick}>
-        {label}
-      </span>
+            <span className={classNames}>
+              <span
+                  className="todo-list-item-label"
+                  style={style}
+                  onClick={this.onLabelClick}>
+                {label}
+              </span>
 
       <button type="button"
               className="btn btn-outline-success btn-sm float-right">
