518daf1b24db12ee633fd84303733512e2f27f0b
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2019-10-14 15:32:17 +0200

    - :facepunch: updates

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ ToDo list exercise
 - `$ yarn start` Starts the development server.
 - `$ yarn build` Bundles the app into static files for production.
 - `$ yarn test` Starts the test runner.
+
+<img src="https://monosnap.com/image/8umtnf45ph7p5Ziv9gG9hWfKjI1KKL"/>
--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -6,7 +6,7 @@ export default class TodoListItem extends Component {
 
     state = {
         done: false,
-        important: false
+        important: false,
     };
 
     onLabelClick = () => {
@@ -26,7 +26,7 @@ export default class TodoListItem extends Component {
     };
 
     render() {
-        const {label = false} = this.props;
+        const {label, onDeleted} = this.props;
         const {done, important} = this.state;
         let classNames = 'todo-list-item';
         if (done) {
@@ -51,7 +51,8 @@ export default class TodoListItem extends Component {
       </button>
 
       <button type="button"
-              className="btn btn-outline-danger btn-sm float-right">
+              className="btn btn-outline-danger btn-sm float-right"
+              onClick={onDeleted}>
         <i className="fa fa-trash-o"/>
       </button>
     </span>
